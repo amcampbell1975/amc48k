f62c0be5834789b65b81d9482ac3649b5389c311
Alistair Campbell <65419433+amcampbell1975@users.noreply.github.com>
2024-03-28 17:02:51 +0000

    Add files via upload

--- amc48k_v1/config.h
+++ amc48k_v1/config.h
@@ -3,7 +3,22 @@
 
 #pragma once
 
-/*
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_IDLE_TIMEOUT 3000
+
+//https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
+#define RGBLIGHT_LAYERS
+
+//https://docs.qmk.fm/#/platformdev_rp2040?id=double-tap
+// The double-tap reset mechanism is an alternate way in QMK to enter the embedded mass storage 
+// UF2 boot-loader of the RP2040. It enables bootloader entry by a fast double-tap of the reset 
+// pin on start up, which is similar to the behavior of AVR Pro Micros. This feature activated 
+// by default for the Pro Micro RP2040 board, but has to be configured for other boards. 
+// To activate it, add the following options to your keyboard
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+ 
+ /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
  */
--- amc48k_v1/rules.mk
+++ amc48k_v1/rules.mk
@@ -1,1 +1,11 @@
-# This file intentionally left blank
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_caps_word.md
+CAPS_WORD_ENABLE = yes
+
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_dynamic_macros.md
+DYNAMIC_MACRO_ENABLE = yes
+
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_command.md
+COMMAND_ENABLE = yes
+
