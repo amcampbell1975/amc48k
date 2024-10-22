59e1dbdf85ac8065b78015bde371e0d413e4d360
Alistair Campbell <65419433+amcampbell1975@users.noreply.github.com>
2024-10-22 20:17:57 +0100

    Enter Space Right Shift Update keymap.c

--- amc48k_v1/keymaps/default/keymap.c
+++ amc48k_v1/keymaps/default/keymap.c
@@ -19,9 +19,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x12(
         QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,  
-        KC_LCTL, MO(3),  KC_LGUI, KC_LALT, MO(1),   KC_ENT,  KC_SPC,  MO(2),   KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,  
+        KC_LCTL, MO(3),   KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_SLASH, KC_LEFT,KC_DOWN, KC_RGHT
     ),
+ 
+ //   Orig [enter] [space] ... [Right Shift]
+ //       KC_LCTL, MO(3),  KC_LGUI, KC_LALT, MO(1),   KC_ENT,  KC_SPC,  MO(2),   KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT  
+ 
     [1] = LAYOUT_ortho_4x12( // UPPER
         _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0, KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_MINS, KC_EQL,
@@ -43,6 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 };
+
 /*
 // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
